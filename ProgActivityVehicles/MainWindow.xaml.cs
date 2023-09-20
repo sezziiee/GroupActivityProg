7b6fb11c19ffa78936e4937223423c72a1e409ab
Zoe <ST10085210@vcconnect.edu.za>
2023-09-20 13:25:47 +0200

    Add project files.

---
+++ ProgActivityVehicles/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VehicleClassLibrary;
+
+
+namespace ProgActivityVehicles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            StreamReader reader = new StreamReader("VehicleData.txt");
+            Vehicle veh = new Vehicle();
+
+            string Line = null;
+            string[] fields;
+
+            int count = 0;
+            while ((Line = reader.ReadLine()) != null)
+            {
+                fields = Line.Split('#');
+                veh.vehicleID = int.Parse(fields[0]);
+                veh.Description = fields[1];
+                veh.vehicleMake = fields[2];
+                veh.numKM = int.Parse(fields[3]);
+                veh.serviceKM = int.Parse(fields[4]);
+                vehicles.Add(veh);
+
+
+                count++;
+            }
+            
+        }
+    }
+}
---
+++ VehicleClassLibrary/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleClassLibrary
+{
+    public class Vehicle
+    {
+        public int vehicleID {  get; set; }
+        public string Description { get; set; }
+        public string vehicleMake { get; set; }
+        public int numKM { get; set; }
+        public int serviceKM { get; set; }
+
+        public string AddVehicle()
+        {
+            return vehicleID + "#" + Description + "#" + vehicleMake + "#" + numKM +"#" + serviceKM;
+    }
+        public override string ToString()
+        {
+            return vehicleID + "\t" + Description + "\t" + vehicleMake + "\t" + numKM + "\t" + serviceKM;
+        }
+    }
+
+    
+}
