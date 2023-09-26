4293f6ad5f90720378dc3d6e6c815976a109adc5
Jarryd Harker <st10209687@vcconnect.edu.za>
2023-09-26 13:36:09 +0200

    Sick Updates

--- ProgActivityVehicles/MainWindow.xaml.cs
+++ ProgActivityVehicles/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,36 +24,52 @@ namespace ProgActivityVehicles
     /// </summary>
     public partial class MainWindow : Window
     {
+        string Path = Directory.GetCurrentDirectory();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             List<Vehicle> vehicles = new List<Vehicle>();
+            Vehicle vehicle = new Vehicle(txtID.Text, txtDesc.Text, txtMake.Text, int.Parse(txtKM.Text), int.Parse(txtServiceKM.Text));
 
-            StreamReader reader = new StreamReader("VehicleData.txt");
-            Vehicle veh = new Vehicle();
+            Directory.CreateDirectory(System.IO.Path.Combine(Path, vehicle.Make));
+            StreamWriter writer = new StreamWriter(System.IO.Path.Combine(Path, vehicle.Make));
+            
+
+            writer.WriteLine(vehicle.AddVehicle());
+
+            vehicles.Add(vehicle);
+
+            writer.Close();
+        }
+
+        private void btnDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            StreamReader reader = new StreamReader(Path);
+            string folder = System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(Path));
+            Vehicle vehicle = new Vehicle();
 
             string Line = null;
             string[] fields;
 
-            int count = 0;
+
             while ((Line = reader.ReadLine()) != null)
             {
                 fields = Line.Split('#');
-                veh.vehicleID = int.Parse(fields[0]);
-                veh.Description = fields[1];
-                veh.vehicleMake = fields[2];
-                veh.numKM = int.Parse(fields[3]);
-                veh.serviceKM = int.Parse(fields[4]);
-                vehicles.Add(veh);
-
-
-                count++;
+                vehicle.ID = fields[0];
+                vehicle.Description = fields[1];
+                vehicle.Make = fields[2];
+                vehicle.KM = int.Parse(fields[3]);
+                vehicle.servicekm = int.Parse(fields[4]);
             }
-            
+
+
+
+            reader.Close();
         }
     }
 }
